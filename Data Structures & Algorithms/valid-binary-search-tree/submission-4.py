fe04f95481302e3167ad56715b59f3ea29da5214
Nnamaka Dike <54998610+Nnamaka@users.noreply.github.com>
2026-06-08 13:26:07 +0100

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+#         q = collections.deque()
+#         q.append((root, float('-inf'), float('inf')))
+
+#         while q:
+#             qlen = len(q)
+
+#             for i in range(qlen):
+#                 node, low, high = q.popleft()
+
+#                 # 1. Validate the current node against its inherited strict boundaries
+#                 if not (low < node.val < high):
+#                     return False
+
+#                 # 2. When appending the left child, update the upper bound to node.val
+#                 if node.left:
+#                     q.append((node.left, low, node.val))
+                    
+#                 # 3. When appending the right child, update the lower bound to node.val
+#                 if node.right:
+#                     q.append((node.right, node.val, high))
+
+#         return True
+
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # using recursive DFS
+
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (node.val < right and node.val > left):
+                return False
+            
+            return (valid(node.left, left, node.val) and 
+            valid(node.right, node.val, right))
+        
+        return valid(root, float("-inf"), float("inf"))
+
+
+

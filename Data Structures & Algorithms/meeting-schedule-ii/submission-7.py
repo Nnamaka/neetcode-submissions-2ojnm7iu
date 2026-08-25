10aad23358a806529da18b3192d53204b0487e88
Nnamaka Dike <54998610+Nnamaka@users.noreply.github.com>
2026-08-25 11:46:52 +0100

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.py
@@ -0,0 +1,54 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+# import heapq
+
+# class Solution:
+#     def minMeetingRooms(self, intervals: List[Interval]) -> int:
+#         if not intervals:
+#             return 0
+
+#         # step 1: Sort meetings by start time
+#         intervals.sort(key=lambda x: x.start)
+
+#         # step 2: Min-heap to store the end times of active meetings
+#         free_rooms = []
+
+#         # Add the end time of the first meeting
+#         heapq.heappush(free_rooms, intervals[0].end)
+
+#         # Step 3: Process remaining meetings
+#         for meeting in intervals[1:]:
+#             # if the room that frees up earliest is ready, reuse it
+#             if meeting.start >= free_rooms[0]:
+#                 heapq.heappop(free_rooms)
+
+#             # Push the current meeting's end time (occupying a room)
+#             heapq.heappush(free_rooms, meeting.end)
+
+#         # The size of the heap is the minimum rooms required
+#         return len(free_rooms)
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        
+        starts = sorted([i.start for i in intervals])
+        ends = sorted([i.end for i in intervals])
+
+        rooms = 0
+        end_ptr = 0
+
+        for start in starts:
+            if start < ends[end_ptr]:
+                rooms += 1
+            else:
+                end_ptr += 1
+        return rooms
+        

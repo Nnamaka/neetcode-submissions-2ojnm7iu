735eb505c2095f4f7d1cd3fdbed6ff2e746adbd6
Nnamaka Dike <54998610+Nnamaka@users.noreply.github.com>
2026-09-15 09:40:59 +0100

    Add: level-order-traversal-of-binary-tree - submission-12

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-12.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+
+        # use python deque datastructure
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qlen = len(q)
+            level = []
+
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            if level:
+                result.append(level)
+        return result
+
+
+
+# # using a list to serve as a queue (FIFO)
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         result = []
+
+#         # first-in-first-out (FIFO)
+#         queue = []
+#         queue.append(root)
+
+#         while queue:
+#             lenq = len(queue)
+#             level = []
+
+#             for i in range(lenq):
+#                 node = queue.pop(0)
+                
+#                 if node:
+#                     level.append(node.val)
+#                     if node.left:
+#                         queue.append(node.left)
+#                     if node.right:
+#                         queue.append(node.right)
+#             if level:
+#                 result.append(level)
+
+#         return result
